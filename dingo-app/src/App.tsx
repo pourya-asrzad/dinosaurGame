83ae8edf8b101ffc7f6eb765bca63af2094cdd3a
puria-asr <poriaasrzad@gmail.com>
2023-04-04 21:32:00 +0430

    bushes added

--- dingo-app/src/App.tsx
+++ dingo-app/src/App.tsx
@@ -1,8 +1,13 @@
 import Dingo from "./components/dingo/Dingo.component";
-
+import bush1 from "./assets/images/bush1.svg";
+import bush2 from "./assets/images/bush2.svg";
+import bush3 from "./assets/images/bush3.svg";
 function App() {
   return (
     <div>
+      <img src={bush1} alt="" />
+      <img src={bush2} alt="" />
+      <img src={bush3} alt="" />
       <Dingo />
     </div>
   );
