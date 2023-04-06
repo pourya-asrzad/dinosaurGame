d344421229b37f9a21b5e32921a4769685f43bd4
puria-asr <poriaasrzad@gmail.com>
2023-04-06 12:02:53 +0430

    jump and bushes move added

--- dingo-app/src/components/yard/Yard.component.tsx
+++ dingo-app/src/components/yard/Yard.component.tsx
@@ -3,16 +3,28 @@ import bush1 from "../../assets/images/bush1.svg";
 import bush2 from "../../assets/images/bush2.svg";
 import bush3 from "../../assets/images/bush3.svg";
 import Dingo from "../dingo/Dingo.component";
+import { useState } from "react";
 const Yard = () => {
+  const [dingoUp, setDingoUp] = useState("dinosaur");
+  document.onkeydown = (e) => {
+    e = e || window.event;
+    if (e.keyCode === 38) {
+      setDingoUp("dinosaurUp");
+      setTimeout(() => {
+        setDingoUp(" ");
+      }, 500);
+    }
+  };
+
   return (
     <div>
       <h1 className={Styles["game-header"]}>dinosaur game 🦕</h1>
       <div className={Styles.Yard}>
         <div className={Styles["playground"]}>
-          <Dingo className={Styles["dinosaur"]} />
+          <Dingo className={Styles["dinosaur"] + " " + Styles[dingoUp]} />
           <div className={Styles["bushes"]}>
-            <img src={bush1} alt="bush1" />
-            <img src={bush2} alt="bush2" />
+            {/* <img src={bush1} alt="bush1" />
+            <img src={bush2} alt="bush2" /> */}
             <img src={bush3} alt="bush3" />
           </div>
           <div className={Styles["hr"]}></div>
